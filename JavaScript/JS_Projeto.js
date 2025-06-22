f2ad3196fef91fc70e1137a40e75ffc784159fc5
Henrique8281 <carlos.henrique87@aluno.ifce.edu.br>
2025-06-22 14:15:15 -0300

    Bootstrap2
    Continuo editando

--- JavaScript/JS_Projeto.js
+++ JavaScript/JS_Projeto.js
@@ -1,1 +1,17 @@
 console.log(teste);
+<script>
+    const form = document.getElementById('recoveryForm');
+    const message = document.getElementById('message');
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const email = document.getElementById('email').value.trim();
+
+      if (email) {
+        message.textContent = 'Se este e-mail estiver cadastrado, você receberá um link para redefinir sua senha.';
+        message.className = 'message success';
+      } else {
+        message.textContent = 'Por favor, preencha o e-mail corretamente.';
+        message.className = 'message error';
+      }
+     </script>
